4dc666d47837c38a0a01db49a39c24f6488be961
admin <admin@MacBook-Pro.local>
2023-12-15 16:23:15 +0300

    button

--- components/CardOne.jsx
+++ components/CardOne.jsx
@@ -2,7 +2,7 @@ const React = require('react');
 
 function CardOne({ card }) {
   return (
-    <div className="cardone" className="card1" data-id={card.id}>
+    <div className="cardone" data-id={card.id}>
       <img src={card.img} style={{ width: '200px' }}></img>
       <a className="a2" href={`/Page/${card.id}`}>
         Изучить
@@ -10,7 +10,10 @@ function CardOne({ card }) {
       <a className="a2" href={`/card/${card.id}`}>
         Изменить
       </a>
-      <button type='button' className='btndelet'> Удалить</button>
+      <button type="button" className="btndelet">
+        {' '}
+        Удалить
+      </button>
     </div>
   );
 }
